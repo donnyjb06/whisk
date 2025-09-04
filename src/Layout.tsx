b8e9703d22d3d8fffbf034ccaff39ff57062c1eb
Donovan Jabbar <djabbar51@gmail.com>
2025-09-04 10:30:29 -0500

    feat: increase animation duration in Layout
    Changed the transition duration for the motion div in Layout.tsx from 0.3 to 0.9 seconds to create a slower animation effect.

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -18,7 +18,7 @@ function Layout() {
 						initial={{opacity: 0, y: 10}}
 						animate={{opacity: 1, y: 0}}
 						exit={{opacity: 0, y: -10}}
-						transition={{duration: 0.3}}
+						transition={{duration: 0.9}}
 						className="flex-1">
 						<Outlet />
 					</motion.div>
