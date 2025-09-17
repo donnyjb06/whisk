6768864b5166b80159d95076222bfdd92cb8ba3b
Donovan Jabbar <djabbar51@gmail.com>
2025-09-16 22:19:25 -0500

    fix(Recipe): remove optional operator from pantry property

--- frontend/src/types/Recipe.ts
+++ frontend/src/types/Recipe.ts
@@ -16,9 +16,8 @@ interface RecipePreferences {
 	allowPantry: boolean;
 	allowExtras: boolean;
 	difficulty: Difficulty;
-	pantry?: { full: string[]; selectedIngredients: string[] };
+	pantry: { full: string[]; selectedIngredients: string[] };
 }
-
 
 interface RecipesContextType {
 	currentRecipe: Recipe | null;
