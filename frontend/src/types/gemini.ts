e7d58c41a6289bcb0e06ea4e7ef4621d6a5aa1b9
Donovan Jabbar <djabbar51@gmail.com>
2025-09-16 22:18:35 -0500

    style(gemini): format file with prettier

--- frontend/src/types/gemini.ts
+++ frontend/src/types/gemini.ts
@@ -1,11 +1,11 @@
 import type { Difficulty } from "./Recipe";
 
 interface PromptInput {
-  ingredients: string[];
-  usePantry?: boolean;
-  allowExtras: boolean;
-  pantry?: string[]
-  difficulty: Difficulty;
+	ingredients: string[];
+	usePantry?: boolean;
+	allowExtras: boolean;
+	pantry?: string[];
+	difficulty: Difficulty;
 }
 
-export type {PromptInput}
+export type { PromptInput };
