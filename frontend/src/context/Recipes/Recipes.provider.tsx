e6345a1fed19a65e22533e28fbb63447b4887586
Donovan Jabbar <djabbar51@gmail.com>
2025-09-19 22:30:52 -0500

    fix: change recipesId property name to recipeIds
    - incorrect property named caused cannot read properties of undefined error for RecipeCard component

--- frontend/src/context/Recipes/Recipes.provider.tsx
+++ frontend/src/context/Recipes/Recipes.provider.tsx
@@ -6,6 +6,9 @@ import { generateRecipe } from "@/services/gemini/generateRecipe";
 import { toast } from "sonner";
 import { v4 as uuidv4 } from "uuid";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { MOCK_RECIPES } from "@/lib/constants";
+
 const RecipesProvider = ({ children }: ChildrenProps) => {
 	const [currentRecipe, setCurrentRecipe] = useState<Recipe | null>(null);
 	const [recipes, setRecipes] = useState<Recipe[]>([]);
@@ -20,11 +23,16 @@ const RecipesProvider = ({ children }: ChildrenProps) => {
 		recipeIdsRef.current = recipeIds;
 
 		if (recipeIdsRef.current.length === 0) return;
-
-		const recipes = recipeIdsRef.current.map((id) => {
-			const recipe = localStorage.getItem(id);
-			if (recipe) return JSON.parse(recipe);
+		/* 		const mock = MOCK_RECIPES.map((recipe) => {
+			localStorage.setItem(recipe._id, JSON.stringify(recipe));
+			return recipe._id;
 		});
+		localStorage.setItem("recipeIds", JSON.stringify(mock)) */ const recipes =
+			recipeIdsRef.current.map((id) => {
+				const recipe = localStorage.getItem(id);
+				if (recipe) return JSON.parse(recipe);
+			});
+		console.log(recipes);
 		setRecipes(recipes);
 	}, []);
 
@@ -76,7 +84,7 @@ const RecipesProvider = ({ children }: ChildrenProps) => {
 	const deleteRecipe = (_id: string) => {
 		recipeIdsRef.current = recipeIdsRef.current?.filter((id) => id !== _id);
 		localStorage.removeItem(_id);
-		localStorage.setItem("recipesId", JSON.stringify(recipeIdsRef.current));
+		localStorage.setItem("recipeIds", JSON.stringify(recipeIdsRef.current));
 
 		setRecipes((prevRecipes) =>
 			prevRecipes.filter((recipe) => recipe._id !== _id)
