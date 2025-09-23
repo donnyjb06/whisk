b3cc0016f33e4d1ce9c6a53e703dc60c97e76c63
Donovan Jabbar <djabbar51@gmail.com>
2025-09-22 22:53:15 -0500

    fix(useUserData): fix consumer hook by subscribing to context

--- frontend/src/hooks/useUserData.ts
+++ frontend/src/hooks/useUserData.ts
@@ -1,12 +1,14 @@
-import { UserDataContext } from "@/context/UserData/UserData.context"
-import { createContext } from "react"
+import { UserDataContext } from "@/context/UserData/UserData.context";
+import { useContext } from "react";
 
 export const useUserData = () => {
-  const context = createContext(UserDataContext);
+	const context = useContext(UserDataContext);
 
-  if (!context) {
-    throw new Error("useUserData must be used within a UserDataProvider component");
-  }
-  
-  return context
-}
+	if (!context) {
+		throw new Error(
+			"useUserData must be used within a UserDataProvider component"
+		);
+	}
+
+	return context;
+};
