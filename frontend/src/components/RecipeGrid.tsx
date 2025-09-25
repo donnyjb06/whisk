6193505c28426e492a1dede37b8a7a44c69ec27c
Donovan Jabbar <djabbar51@gmail.com>
2025-09-24 23:34:15 -0500

    feat(RecipeGrid): accept className prop
    - allows styling customization from parent components

--- frontend/src/components/RecipeGrid.tsx
+++ frontend/src/components/RecipeGrid.tsx
@@ -1,14 +1,22 @@
 import { useRecipes } from "@/hooks/useRecipes";
 import RecipeCard from "./RecipeCard";
 import { AnimatePresence, motion } from "motion/react";
+import { cn } from "@/lib/utils";
 
-const RecipeGrid = () => {
+interface RecipeGridProps {
+	className?: string;
+}
+
+const RecipeGrid = ({ className }: RecipeGridProps) => {
 	const { recipes } = useRecipes();
 	return (
 		<motion.div
 			layout
-			transition={{layout: { type: "spring", stiffness: 300, damping: 25}}}
-			className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
+			transition={{ layout: { type: "spring", stiffness: 300, damping: 25 } }}
+			className={cn(
+				"grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4",
+				className
+			)}
 		>
 			<AnimatePresence>
 				{recipes.map((recipe) => (
