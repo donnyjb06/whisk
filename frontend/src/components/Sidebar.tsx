36af915adb75e383c2d8a5d7d55eb257e316462c
Donovan Jabbar <djabbar51@gmail.com>
2025-09-29 21:28:38 -0500

    fix(sidebar): fix NavLink isActive state
    pass end prop to NavLink component to only set activate isActive when end of pathname matches

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,9 +1,10 @@
 import { useUserData } from "@/hooks/useUserData";
+import { cn } from "@/lib/utils";
 import { NavLink, redirect } from "react-router";
 
 const navItems = [
 	{
-		to: "information",
+		to: "/profile",
 		label: "Edit Profile",
 	},
 	{
@@ -21,17 +22,21 @@ const Sidebar = () => {
 
 	if (!currentUser) redirect("/");
 	return (
-		<div className="flex flex-col gap-4 lg:gap-6 items-start pb-6 lg:pr-6 lg:pb-0 lg:max-w-1/3 border-b-2 lg:border-b-0 lg:border-r-2">
-			<h4 className="heading4">{currentUser?.name}</h4>
+		<div className="flex flex-col gap-4 items-start pb-6 md:pr-6 md:pb-0 min-w-1/4 border-b-2 md:border-b-0 md:border-r-2">
+			<h4 className="heading5 lg:heading4 whitespace-nowrap">
+				{currentUser?.name}
+			</h4>
 			<div className="flex flex-col gap-1 lg:gap-2">
 				{navItems.map((item) => (
 					<NavLink
+						key={item.to}
+						end
 						to={item.to}
 						className={({ isActive }) =>
-							[
-								isActive ? "text-primary" : "",
+							cn(
 								"buttontext hover:text-muted transition-colors duration-200",
-							].join("")
+								isActive && "text-primary"
+							)
 						}
 					>
 						{item.label}
