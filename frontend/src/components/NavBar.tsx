468f835eb2509a2dabd1fa43438dcb79f3f97896
Donovan Jabbar <djabbar51@gmail.com>
2025-09-30 23:09:31 -0500

    fix(mobilenavmenu): use Link component over anchor tags

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -13,6 +13,7 @@ import {
 import ModeToggle from "./ModeToggle";
 import { useModal } from "@/hooks/useModal";
 import { useUserData } from "@/hooks/useUserData";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
 	const navItems = [
@@ -67,14 +68,14 @@ const NavBar = () => {
 					onClose={() => setIsMobileMenuOpen(false)}
 				>
 					{navItems.map((item, idx) => (
-						<a
+						<Link
 							key={`mobile-link-${idx}`}
-							href={item.link}
+							to={item.link}
 							onClick={() => setIsMobileMenuOpen(false)}
 							className="relative text-neutral-600 dark:text-neutral-300 buttontext"
 						>
 							<span className="block">{item.name}</span>
-						</a>
+						</Link>
 					))}
 					<div className="flex w-full flex-col gap-4">
 						{!currentUser && (
