deb5c43094d090d0fb72ad07f9212010e92dcced
Donovan Jabbar <djabbar51@gmail.com>
2025-09-30 21:59:31 -0500

    feat(ProfilePage): add page transition to nested routes

--- frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,11 +1,18 @@
+import PageTransition from "@/components/PageTransition";
 import Sidebar from "@/components/Sidebar";
+import { useLocation } from "react-router";
 import { Outlet } from "react-router";
 
 const ProfilePage = () => {
+	const location = useLocation();
+	const nestedLevel = location.pathname.split("/")[2] || "/";
+
 	return (
 		<main className="overflow-hidden grow max-w-7xl mx-auto min-h-screen flex flex-col p-6 gap-6 md:flex-row">
 			<Sidebar />
-			<Outlet />
+			<PageTransition className="grow" pathName={nestedLevel}>
+				<Outlet />
+			</PageTransition>
 		</main>
 	);
 };
