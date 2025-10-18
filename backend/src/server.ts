ab8916d990af63a630553e9c61fd695e58e9d953
Donovan Jabbar <djabbar51@gmail.com>
2025-10-18 13:50:15 -0500

    chore(middleware): setup cors and json middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,17 @@
-import express from "express";
+import express, { json, type Express } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express();
-const { PORT = 3001 } = process.env;
+dotenv.config();
+
+const app: Express = express();
+const { PORT = 3001, DATABASE_CONNECTION_URI = "" } = process.env;
+
+app.use(cors());
+app.use(json());
+
+(async () => mongoose.connect(DATABASE_CONNECTION_URI))();
 
 app.listen(PORT, () => {
 	console.log("Listening to the server");
